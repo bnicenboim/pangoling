4955909e1472cc5ca358ceace05965fb2ae17368
bnicenboim <bruno.nicenboim@gmail.com>
2023-06-06 16:21:06 +0200

    removing a test for long input

--- tests/testthat/test-tr_causal.R
+++ tests/testthat/test-tr_causal.R
@@ -32,18 +32,20 @@ test_that("empty or small strings", {
   expect_equal(lp_small, c(NA_real_, "It" = NA_real_))
 })
 
-
+if(0){
+  #long inputs require too much memory
 test_that("long input work", {
   skip_if_no_python_stuff()
   long0 <- paste(rep("x", 1022), collapse = " ")
   long <- paste(rep("x", 1024), collapse = " ")
   longer <- paste(rep("x", 1025), collapse = " ")
-  lp_long0 <- causal_tokens_lp_tbl(c(long0, long, longer), add_special_tokens = TRUE, batch_size = 3, model = "sshleifer/tiny-gpt2")
+  lp_long0 <- causal_tokens_lp_tbl(texts = c(long0, long, longer), add_special_tokens = TRUE, batch_size = 3, model = "sshleifer/tiny-gpt2")
   skip_on_os("windows") #the following just doesn't work on windows,
   # but it's not that important
     lp_long1 <- causal_tokens_lp_tbl(c(long0, long, longer), add_special_tokens = TRUE, batch_size = 1, model = "sshleifer/tiny-gpt2")
   expect_equal(lp_long0, lp_long1)
 })
+}
 
 test_that("errors work", {
   skip_if_no_python_stuff()
